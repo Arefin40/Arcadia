2136ea71ebfef09ddcbc57ec325e100ec00aafc6
ShahriarArefin <arfin17310@gmail.com>
2024-03-27 02:26:04 +0600

    export all icons

---
+++ src/icons/index.js
@@ -0,0 +1,12 @@
+export { default as Facebook } from "./Facebook";
+export { default as Twitter } from "./Twitter";
+export { default as Instagram } from "./Instagram";
+export { default as Github } from "./Github";
+
+export { default as Publisher } from "./Publisher";
+export { default as Menu } from "./Menu";
+export { default as Cross } from "./Cross";
+export { default as Calendar } from "./Calendar";
+export { default as Pages } from "./Pages";
+export { default as Star } from "./Star";
+export { default as Arrow } from "./Arrow";
